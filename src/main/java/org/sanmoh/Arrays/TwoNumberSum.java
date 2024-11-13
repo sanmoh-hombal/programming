232282b6cca94557affdcb34cfdecdee19fade21
Sanmoh Hombal <sanmoh.hombal@gmail.com>
2024-11-13 19:10:46 +0530

    ✨ docs: Add problem statement and example to TwoNumberSum class ✏️

--- src/main/java/org/sanmoh/Arrays/TwoNumberSum.java
+++ src/main/java/org/sanmoh/Arrays/TwoNumberSum.java
@@ -1,3 +1,27 @@
 package org.sanmoh.Arrays;
 
+/**
+ * <h1>Two Number Sum
+ *
+ * <h2>Problem Statement
+ *
+ * <p>Write a function that takes in a non-empty array of distinct integers and an integer
+ * representing a target sum. If any two numbers in the input array sum up to the target sum, the
+ * function should return them in an array, in any order. If no two numbers sum up to the target
+ * sum, the function should return an empty array.
+ *
+ * <p>Note that the target sum has to be obtained by summing two different integers in the array;
+ * you can't add a single integer to itself in order to obtain the target sum.
+ *
+ * <h3>Sample Input
+ *
+ * <pre>
+ *     array = [3, 5, -4, 8, 11, 1, -1, 6]
+ *     targetSum = 10
+ *
+ * <h3>Sample Output
+ *
+ * <pre>
+ *     [-1, 11] // the numbers could be in reverse order
+ */
 public class TwoNumberSum {}
