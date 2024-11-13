03e0aa0e27a07a739385a679995fde3de794be3f
Sanmoh Hombal <sanmoh.hombal@gmail.com>
2024-11-13 21:39:07 +0530

    💡 docs: Add class-level documentation for `ValidateSubsequence` algorithm

--- src/main/java/org/sanmoh/Arrays/ValidateSubsequence.java
+++ src/main/java/org/sanmoh/Arrays/ValidateSubsequence.java
@@ -1,3 +1,25 @@
 package org.sanmoh.Arrays;
 
+/**
+ * Validate Subsequence
+ *
+ * <h2>Problem Statement
+ *
+ * <p>Given two non-empty arrays of integers, write a function that determines whether the second
+ * array is a subsequence of the first one.
+ *
+ * <p>A subsequence of an array is a set of numbers that aren't necessarily adjacent in the array
+ * but that are in the same order as they appear in the array. For instance, the numbers.
+ *
+ * <h3>Sample Input
+ *
+ * <pre>
+ *     array = [5, 1, 22, 25, 6, -1, 8, 10]
+ *     sequence = [1, 6, -1, 10]
+ *
+ * <h3>Sample Output
+ *
+ * <pre>
+ *     true
+ */
 public class ValidateSubsequence {}
